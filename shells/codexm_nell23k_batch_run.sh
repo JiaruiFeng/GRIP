cec9556b91d2672ed21848b407373e13361207cd
Cai <cai.d@compute1-client-1.ris.wustl.edu>
2025-10-02 13:15:00 -0500

    Add run scripts

---
+++ shells/codexm_nell23k_batch_run.sh
@@ -0,0 +1,165 @@
+#PYTHONPATH=. python scripts/mp_wrapper.py \
+#    --script scripts/run_llm.py \
+#    --num_process 1 \
+#    --dataset codexm \
+#    --output_file codexm_qwen_baseline_submit_nocontext.json \
+#    --do_eval \
+#    --metrics em f1 hit \
+#    --llm_as_judge_model qwen-32b \
+#    --report_to_wandb \
+#    --wandb_project_name grip \
+#    --wandb_run_name codexm_qwen_baseline_submit_nocontext \
+#    --subprocess_args \
+#    --overwrite True \
+#    --tokenize_max_length 30000 \
+#    --gen_max_length 1000 \
+#    --padding_side left \
+#    --truncation_side left \
+#    --model_name qwen-7b \
+#    --dtype bfloat16 \
+#    --use_vllm True \
+#    --batch_size -1 \
+#    --do_sample True \
+#    --top_p 0.9 \
+#    --temperature 0.6 \
+#    --no_graph_context True \
+#    --use_subgraph True \
+#    --index_format False \
+#    --report_input_token_count True > logs/qwen_llm_inf_codexm_nocontext.log 2>&1
+#
+#PYTHONPATH=. python scripts/mp_wrapper.py \
+#    --script scripts/run_llm.py \
+#    --num_process 1 \
+#    --dataset nell23k \
+#    --output_file nell23k_qwen_baseline_submit_nocontext.json \
+#    --do_eval \
+#    --metrics em f1 hit \
+#    --llm_as_judge_model qwen-32b \
+#    --report_to_wandb \
+#    --wandb_project_name grip \
+#    --wandb_run_name nell23k_qwen_baseline_submit_nocontext \
+#    --subprocess_args \
+#    --overwrite True \
+#    --tokenize_max_length 30000 \
+#    --gen_max_length 1000 \
+#    --padding_side left \
+#    --truncation_side left \
+#    --model_name qwen-7b \
+#    --dtype bfloat16 \
+#    --use_vllm True \
+#    --batch_size -1 \
+#    --do_sample True \
+#    --top_p 0.9 \
+#    --temperature 0.6 \
+#    --no_graph_context True \
+#    --use_subgraph True \
+#    --index_format False \
+#    --report_input_token_count True > logs/qwen_llm_inf_nell23k_nocontext.log 2>&1
+#
+#PYTHONPATH=. python scripts/mp_wrapper.py \
+#    --script scripts/run_llm.py \
+#    --num_process 1 \
+#    --dataset codexm \
+#    --output_file codexm_llama_baseline_context_submit.json \
+#    --do_eval \
+#    --metrics em f1 hit \
+#    --llm_as_judge_model qwen-32b \
+#    --report_to_wandb \
+#    --wandb_project_name grip \
+#    --wandb_run_name codexm_llama_baseline_context_submit \
+#    --subprocess_args \
+#    --overwrite True \
+#    --tokenize_max_length 30000 \
+#    --gen_max_length 1000 \
+#    --padding_side left \
+#    --truncation_side left \
+#    --model_name llama3-8b \
+#    --dtype bfloat16 \
+#    --use_vllm True \
+#    --do_sample True \
+#    --top_p 0.9 \
+#    --temperature 0.6 \
+#    --no_graph_context False \
+#    --use_subgraph True \
+#    --index_format False > logs/llama3_llm_inf_codexm.log 2>&1
+#
+#PYTHONPATH=. python scripts/mp_wrapper.py \
+#    --script scripts/run_llm.py \
+#    --num_process 1 \
+#    --dataset nell23k \
+#    --output_file nell23k_llama_baseline_context_submit.json \
+#    --do_eval \
+#    --metrics em f1 hit \
+#    --llm_as_judge_model qwen-32b \
+#    --report_to_wandb \
+#    --wandb_project_name grip \
+#    --wandb_run_name nell23k_llama_baseline_context_submit \
+#    --subprocess_args \
+#    --overwrite True \
+#    --tokenize_max_length 30000 \
+#    --gen_max_length 1000 \
+#    --padding_side left \
+#    --truncation_side left \
+#    --model_name llama3-8b \
+#    --dtype bfloat16 \
+#    --use_vllm True \
+#    --do_sample True \
+#    --top_p 0.9 \
+#    --temperature 0.6 \
+#    --no_graph_context False \
+#    --use_subgraph True \
+#    --index_format False > logs/llama3_llm_inf_nell23k.log 2>&1
+#
+#PYTHONPATH=. python scripts/mp_wrapper.py \
+#    --script scripts/run_llm.py \
+#    --num_process 1 \
+#    --dataset codexm \
+#    --output_file codexm_llama_baseline_context_submit_nocontext.json \
+#    --do_eval \
+#    --metrics em f1 hit \
+#    --llm_as_judge_model qwen-32b \
+#    --report_to_wandb \
+#    --wandb_project_name grip \
+#    --wandb_run_name codexm_llama_baseline_context_submit_nocontext \
+#    --subprocess_args \
+#    --overwrite True \
+#    --tokenize_max_length 30000 \
+#    --gen_max_length 1000 \
+#    --padding_side left \
+#    --truncation_side left \
+#    --model_name llama3-8b \
+#    --dtype bfloat16 \
+#    --use_vllm True \
+#    --do_sample True \
+#    --top_p 0.9 \
+#    --temperature 0.6 \
+#    --no_graph_context True \
+#    --use_subgraph True \
+#    --index_format False > logs/llama3_llm_inf_codexm_nocontext.log 2>&1
+
+PYTHONPATH=. python scripts/mp_wrapper.py \
+    --script scripts/run_llm.py \
+    --num_process 1 \
+    --dataset nell23k \
+    --output_file nell23k_llama_baseline_context_submit.json_nocontext \
+    --do_eval \
+    --metrics em f1 hit \
+    --llm_as_judge_model qwen-32b \
+    --report_to_wandb \
+    --wandb_project_name grip \
+    --wandb_run_name nell23k_llama_baseline_context_submit_nocontext \
+    --subprocess_args \
+    --overwrite True \
+    --tokenize_max_length 30000 \
+    --gen_max_length 1000 \
+    --padding_side left \
+    --truncation_side left \
+    --model_name llama3-8b \
+    --dtype bfloat16 \
+    --use_vllm True \
+    --do_sample True \
+    --top_p 0.9 \
+    --temperature 0.6 \
+    --no_graph_context True \
+    --use_subgraph True \
+    --index_format False > logs/llama3_llm_inf_nell23k_nocontext.log 2>&1
---
+++ shells/qwen_llm_inf/nell23k.sh
@@ -0,0 +1,30 @@
+PYTHONPATH=. python scripts/mp_wrapper.py \
+    --script scripts/run_llm.py \
+    --num_process 1 \
+    --dataset nell23k \
+    --output_file nell23k_qwen_baseline_submit.json \
+    --do_eval \
+    --metrics em f1 hit \
+    --llm_as_judge_model qwen-32b \
+    --report_to_wandb \
+    --wandb_project_name grip \
+    --wandb_run_name nell23k_qwen_baseline_submit \
+    --subprocess_args \
+    --overwrite True \
+    --tokenize_max_length 30000 \
+    --gen_max_length 1000 \
+    --padding_side left \
+    --truncation_side left \
+    --model_name qwen-7b \
+    --dtype bfloat16 \
+    --use_vllm True \
+    --batch_size -1 \
+    --do_sample True \
+    --top_p 0.9 \
+    --temperature 0.6 \
+    --no_graph_context False \
+    --use_subgraph True \
+    --index_format False \
+    --report_input_token_count True
+
+
