3716e1a2e4a1f801840ca0d465fc5c7e022115fe
Cai <cai.d@compute1-client-1.ris.wustl.edu>
2025-10-04 17:38:18 -0500

    convert fb edge to readable edge

---
+++ data/raw_datasets/fb15k237_2/process.py
@@ -0,0 +1,113 @@
+import json
+from os import path as osp
+
+import numpy as np
+
+from utils import make_dir, download_hf_file, save_list_json
+
+HF_REPO_ID = "WFRaain/TAG_datasets"
+
+
+def process_fb15k237(save_dir: str):
+    # make_dir(save_dir)
+    if not osp.exists(osp.join(save_dir, "entity2wikidata.json")):
+        download_hf_file(HF_REPO_ID, subfolder="FB15K237", filename="entity2wikidata.json", local_dir=save_dir)
+    if not osp.exists(osp.join(save_dir, "train.txt")):
+        download_hf_file(HF_REPO_ID, subfolder="FB15K237", filename="train.txt", local_dir=save_dir)
+    if not osp.exists(osp.join(save_dir, "test.txt")):
+        download_hf_file(HF_REPO_ID, subfolder="FB15K237", filename="test.txt", local_dir=save_dir)
+    if not osp.exists(osp.join(save_dir, "valid.txt")):
+        download_hf_file(HF_REPO_ID, subfolder="FB15K237", filename="valid.txt", local_dir=save_dir)
+    if not osp.exists(osp.join(save_dir, "fb15k237.json")):
+        download_hf_file(HF_REPO_ID, subfolder="FB15K237", filename="fb15k237.json", local_dir=save_dir)
+
+    entity2text = {}
+    with open(osp.join(save_dir, "entity2wikidata.json"), "r") as f:
+        data = json.load(f)
+
+    with open(osp.join(save_dir, "rel2text.json"), "r") as f:
+        rel_text_dict = json.load(f)
+
+    for k in data:
+        entity = ""
+        if data[k]["label"] is not None:
+            entity += data[k]["label"] + ", "
+        if data[k]["description"] is not None:
+            entity += data[k]["description"]
+        if not entity:
+            entity = "missing"
+        entity2text[k] = entity.strip().strip(".")
+    entity2id = {entity: i for i, entity in enumerate(entity2text.keys())}
+
+    file_path = osp.join(save_dir, "train.txt")
+    edge_list = []
+    edge_index = []
+    unique_rel = set()
+
+    with open(file_path) as f:
+        file_data = [line.split() for line in f.read().split("\n")[:-1]]
+    unknown_entity = 0
+    for triplet in file_data:
+        if triplet[0] not in entity2id:
+            entity2text[triplet[0]] = "Unknown"
+            entity2id[triplet[0]] = len(entity2id)
+            unknown_entity += 1
+        if triplet[2] not in entity2id:
+            entity2text[triplet[2]] = "Unknown"
+            entity2id[triplet[2]] = len(entity2id)
+            unknown_entity += 1
+        rel = triplet[1]
+        # convert rel to meaningful text
+        rel = rel_text_dict[rel]
+        unique_rel.add(rel)
+        edge_list.append([entity2text[triplet[0]], rel, entity2text[triplet[2]]])
+        edge_index.append([entity2id[triplet[0]], entity2id[triplet[2]]])
+
+    graph = {
+        "edge_index": edge_index,
+        "edge_list": edge_list,
+        "node_list": list(entity2text.values()),
+    }
+    unique_rel = list(unique_rel)
+    WAY = 10
+    Q_format = ("What is the relation between node {src} and node {tgt}? "
+                "Selected from the following candidate answers: {candidates}.")
+    for name in ["valid", "test"]:
+        file_path = osp.join(save_dir, name + ".txt")
+        with open(file_path) as f:
+            file_data = [line.split() for line in f.read().split("\n")[:-1]]
+        unknown_entity = 0
+        questions = []
+        answers = []
+        for triplet in file_data:
+            if triplet[0] not in entity2id:
+                entity2text[triplet[0]] = "Unknown"
+                entity2id[triplet[0]] = len(entity2id)
+                unknown_entity += 1
+            if triplet[2] not in entity2id:
+                entity2text[triplet[2]] = "Unknown"
+                entity2id[triplet[2]] = len(entity2id)
+                unknown_entity += 1
+            rel = triplet[1]
+            # convert rel to meaningful text
+            rel = rel_text_dict[rel]
+            unique_rel_copy = unique_rel.copy()
+            unique_rel_copy.remove(rel)
+            negative = np.random.permutation(unique_rel_copy)[:WAY - 1]
+            candidates = "; ".join(np.random.permutation(negative.tolist() + [rel]))
+            q = Q_format.format(src=entity2text[triplet[0]], tgt=entity2text[triplet[2]], candidates=candidates)
+            a = rel
+            questions.append([q, [entity2id[triplet[0]], entity2id[triplet[2]]]])
+            answers.append(a)
+        # random select 10000
+        selected_index = np.random.choice(len(questions), 10000, replace=False)
+        questions = [questions[i] for i in selected_index]
+        answers = [answers[i] for i in selected_index]
+        data_list = [{"title": "fb15k237_2", "graph": graph, "questions": questions, "answers": answers, "id": "0"}]
+        if name == "valid":
+            name = "val"
+        save_list_json(osp.join(save_dir, f"processed_{name}.json"), data_list)
+
+
+if __name__ == "__main__":
+    process_fb15k237("outputs/data/fb15k237_2")
--- shells/codexm_nell23k_batch_run.sh
+++ shells/codexm_nell23k_batch_run.sh
@@ -56,98 +56,73 @@
 #    --index_format False \
 #    --report_input_token_count True > logs/qwen_llm_inf_nell23k_nocontext.log 2>&1
 #
-#PYTHONPATH=. python scripts/mp_wrapper.py \
-#    --script scripts/run_llm.py \
-#    --num_process 1 \
-#    --dataset codexm \
-#    --output_file codexm_llama_baseline_context_submit.json \
-#    --do_eval \
-#    --metrics em f1 hit \
-#    --llm_as_judge_model qwen-32b \
-#    --report_to_wandb \
-#    --wandb_project_name grip \
-#    --wandb_run_name codexm_llama_baseline_context_submit \
-#    --subprocess_args \
-#    --overwrite True \
-#    --tokenize_max_length 30000 \
-#    --gen_max_length 1000 \
-#    --padding_side left \
-#    --truncation_side left \
-#    --model_name llama3-8b \
-#    --dtype bfloat16 \
-#    --use_vllm True \
-#    --do_sample True \
-#    --top_p 0.9 \
-#    --temperature 0.6 \
-#    --no_graph_context False \
-#    --use_subgraph True \
-#    --index_format False > logs/llama3_llm_inf_codexm.log 2>&1
-#
-#PYTHONPATH=. python scripts/mp_wrapper.py \
-#    --script scripts/run_llm.py \
-#    --num_process 1 \
-#    --dataset nell23k \
-#    --output_file nell23k_llama_baseline_context_submit.json \
-#    --do_eval \
-#    --metrics em f1 hit \
-#    --llm_as_judge_model qwen-32b \
-#    --report_to_wandb \
-#    --wandb_project_name grip \
-#    --wandb_run_name nell23k_llama_baseline_context_submit \
-#    --subprocess_args \
-#    --overwrite True \
-#    --tokenize_max_length 30000 \
-#    --gen_max_length 1000 \
-#    --padding_side left \
-#    --truncation_side left \
-#    --model_name llama3-8b \
-#    --dtype bfloat16 \
-#    --use_vllm True \
-#    --do_sample True \
-#    --top_p 0.9 \
-#    --temperature 0.6 \
-#    --no_graph_context False \
-#    --use_subgraph True \
-#    --index_format False > logs/llama3_llm_inf_nell23k.log 2>&1
-#
-#PYTHONPATH=. python scripts/mp_wrapper.py \
-#    --script scripts/run_llm.py \
-#    --num_process 1 \
-#    --dataset codexm \
-#    --output_file codexm_llama_baseline_context_submit_nocontext.json \
-#    --do_eval \
-#    --metrics em f1 hit \
-#    --llm_as_judge_model qwen-32b \
-#    --report_to_wandb \
-#    --wandb_project_name grip \
-#    --wandb_run_name codexm_llama_baseline_context_submit_nocontext \
-#    --subprocess_args \
-#    --overwrite True \
-#    --tokenize_max_length 30000 \
-#    --gen_max_length 1000 \
-#    --padding_side left \
-#    --truncation_side left \
-#    --model_name llama3-8b \
-#    --dtype bfloat16 \
-#    --use_vllm True \
-#    --do_sample True \
-#    --top_p 0.9 \
-#    --temperature 0.6 \
-#    --no_graph_context True \
-#    --use_subgraph True \
-#    --index_format False > logs/llama3_llm_inf_codexm_nocontext.log 2>&1
-
 PYTHONPATH=. python scripts/mp_wrapper.py \
     --script scripts/run_llm.py \
     --num_process 1 \
-    --dataset nell23k \
-    --output_file nell23k_llama_baseline_context_submit.json_nocontext \
+    --dataset codexm \
+    --output_file codexm_llama_baseline_context_submit_rerun.json \
     --do_eval \
     --metrics em f1 hit \
     --llm_as_judge_model qwen-32b \
     --report_to_wandb \
     --wandb_project_name grip \
-    --wandb_run_name nell23k_llama_baseline_context_submit_nocontext \
+    --wandb_run_name codexm_llama_baseline_context_submit_rerun \
+    --subprocess_args \
+    --overwrite True \
+    --tokenize_max_length 30000 \
+    --gen_max_length 1000 \
+    --padding_side left \
+    --truncation_side left \
+    --model_name llama3-8b \
+    --dtype bfloat16 \
+    --use_vllm True \
+    --do_sample True \
+    --top_p 0.9 \
+    --temperature 0.6 \
+    --no_graph_context False \
+    --use_subgraph True \
+    --index_format False \
+    --report_input_token_count True > logs/llama3_llm_inf_codexm_rerun.log 2>&1
+
+PYTHONPATH=. python scripts/mp_wrapper.py \
+    --script scripts/run_llm.py \
+    --num_process 1 \
+    --dataset nell23k \
+    --output_file nell23k_llama_baseline_context_submit_rerun.json \
+    --do_eval \
+    --metrics em f1 hit \
+    --llm_as_judge_model qwen-32b \
+    --report_to_wandb \
+    --wandb_project_name grip \
+    --wandb_run_name nell23k_llama_baseline_context_submit_rerun \
+    --subprocess_args \
+    --overwrite True \
+    --tokenize_max_length 30000 \
+    --gen_max_length 1000 \
+    --padding_side left \
+    --truncation_side left \
+    --model_name llama3-8b \
+    --dtype bfloat16 \
+    --use_vllm True \
+    --do_sample True \
+    --top_p 0.9 \
+    --temperature 0.6 \
+    --no_graph_context False \
+    --use_subgraph True \
+    --index_format False \
+    --report_input_token_count True > logs/llama3_llm_inf_nell23k_rerun.log 2>&1
+
+PYTHONPATH=. python scripts/mp_wrapper.py \
+    --script scripts/run_llm.py \
+    --num_process 1 \
+    --dataset codexm \
+    --output_file codexm_llama_baseline_context_submit_nocontext_rerun.json \
+    --do_eval \
+    --metrics em f1 hit \
+    --llm_as_judge_model qwen-32b \
+    --report_to_wandb \
+    --wandb_project_name grip \
+    --wandb_run_name codexm_llama_baseline_context_submit_nocontext_rerun \
     --subprocess_args \
     --overwrite True \
     --tokenize_max_length 30000 \
@@ -162,4 +137,33 @@ PYTHONPATH=. python scripts/mp_wrapper.py \
     --temperature 0.6 \
     --no_graph_context True \
     --use_subgraph True \
-    --index_format False > logs/llama3_llm_inf_nell23k_nocontext.log 2>&1
+    --index_format False \
+    --report_input_token_count True > logs/llama3_llm_inf_codexm_nocontext_rerun.log 2>&1
+
+PYTHONPATH=. python scripts/mp_wrapper.py \
+    --script scripts/run_llm.py \
+    --num_process 1 \
+    --dataset nell23k \
+    --output_file nell23k_llama_baseline_context_submit.json_nocontext_rerun \
+    --do_eval \
+    --metrics em f1 hit \
+    --llm_as_judge_model qwen-32b \
+    --report_to_wandb \
+    --wandb_project_name grip \
+    --wandb_run_name nell23k_llama_baseline_context_submit_nocontext_rerun \
+    --subprocess_args \
+    --overwrite True \
+    --tokenize_max_length 30000 \
+    --gen_max_length 1000 \
+    --padding_side left \
+    --truncation_side left \
+    --model_name llama3-8b \
+    --dtype bfloat16 \
+    --use_vllm True \
+    --do_sample True \
+    --top_p 0.9 \
+    --temperature 0.6 \
+    --no_graph_context True \
+    --use_subgraph True \
+    --index_format False \
+    --report_input_token_count True > logs/llama3_llm_inf_nell23k_nocontext_rerun.log 2>&1
